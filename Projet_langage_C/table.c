a5bbbac76c3ee0c8671a8e25711988eafde73e72
Guillaume <guillaume.stasinski@gmail.com>
2017-12-09 12:09:03 +0100

    V1.10.2

--- Projet_langage_C/table.c
+++ Projet_langage_C/table.c
@@ -43,12 +43,101 @@ int index_calculator(char* name, char* surname, unsigned int base, unsigned int 
 	sum %= N;
 	free(name_to_use);
 	name_to_use = NULL;
+    create_table(N, base);
 	return (int) sum;
 }
 
 civil_servant** create_table(unsigned int size_table, unsigned int base)
 {
-	civil_servant * * const table = malloc(size_table*sizeof(civil_servant));
+	civil_servant * * table = malloc(size_table*sizeof(civil_servant));
 	for(int i = 0; i < size_table; i++) { table[i] = NULL; }
-	return table;
+    
+    /*
+    civil_servant * new_civil = malloc(sizeof(civil_servant));
+    new_civil->name = "carol";
+    printf("%s", new_civil->name);
+    table[1] = new_civil;
+    printf("%s", table[1]->name);
+     * 
+     */ //Ceci est un essai pour voir comment cela marche
+    return table;
+
 }
+
+void add(char* name, char* surname, unsigned int salary, civil_servant* civil){
+    	if(civil->next == NULL)
+	{
+		civil_servant * new_civil = malloc(sizeof(civil_servant));
+		civil->next = new_civil;
+		new_civil->next = NULL;
+		new_civil->surname = surname;
+		new_civil->name = name;
+		new_civil->salary = salary;
+	}
+	else 
+	{
+		add(name, surname, salary,  civil->next);
+	}
+}
+    int show_salary(char* name, char* surname, civil_servant * civil)
+    {
+        if( civil-> next != NULL)
+        {
+            if (civil->name == name && civil->surname == surname)
+            {
+                return civil->salary;
+            }
+            else
+            {
+                show_salary(name, surname, civil->next);
+            }
+        }
+        else
+        {
+            return -1;
+        }
+        
+        
+    }
+    int show_salary_between(int first_index, int end_index,civil_servant ** table)
+    {
+        for (int i =first_index; i<=end_index; i++)
+        {
+            while(table[i]->salary != NULL) 
+            {
+                printf("le salaire de %s %s est de %d",table[i]->name, table[i]->surname, table[i]->salary);
+                table[i] = table[i]->next;// Il faut réussir à passer à table->next; Formule adéquate ? 
+            }
+        }
+    }
+    
+    void delete_civil(char* name, char* surname,civil_servant ** table )
+    {
+         for (int i = 0; i<=100; i++) // index de la table à modofier
+        {
+            while(table[i]->name != NULL) 
+            {
+                if (table[i]->name == name && table[i]->surname == surname)
+                {
+                    if (table[i]->next == NULL)
+                    {
+                        free(table[i]);
+                    }
+                    else
+                    {
+                        civil_servant * index = table[i];
+                        		while(index->next->next != NULL)
+                            {
+                            index = index->next;
+                            }
+
+                        free(table[i]->next);
+                        table[i]->next = NULL;
+                    }
+                    
+                }
+            }
+        
+        }
+    
+    }
--- Projet_langage_C/table.h
+++ Projet_langage_C/table.h
@@ -20,13 +20,13 @@ typedef struct _civil_servant
 
 int index_calculator(char* name, char* surname, unsigned int base,unsigned int N);
 unsigned long long power (int number, int power);
-void add(char* name, char* surname, unsigned int salary);
+void add(char* name, char* surname, unsigned int salary, civil_servant* civil); // Il faut calculer l'index en dehors et chercher le dernier civil_servant associé à cet index.
 void load(int number_of_servant);
-int show_salary(char* name, char* surname);
-int show_salary_between(int first_index, int end_index);
+int show_salary(char* name, char* surname, civil_servant * civil);
+int show_salary_between(int first_index, int end_index, civil_servant ** table);
 int conflict();
 int average_conflict();
-void delete_civil(char* name, char* surname);
+void delete_civil(char* name, char* surname,civil_servant ** table );
 void delete_civil_between(char* name, char* surname,int first_index, int end_index);
 void leave();
 civil_servant** create_table(unsigned int size_table, unsigned int base);
