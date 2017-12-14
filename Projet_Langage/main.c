035100784feb452c3730030e6bad347c3bbdcce3
Guillaume <guillaume.stasinski@gmail.com>
2017-12-14 13:07:47 +0100

    v2.51
    log (13h03)
    J'ai enfin réussir à écrire
    log(13h06)
    J'ai un nouveau problème, la table invente des index, à la recherche du problème
    Terminé

--- Projet_Langage/main.c
+++ Projet_Langage/main.c
@@ -6,23 +6,31 @@ int main(int argc, char **argv)
 {
     table.size = 100;
     table.base = 83;
-    int index = index_calculator("AARON", "Kimberlei");
+  //  int index = index_calculator("AARON", "Kimberlei");
     create_table();
-    add("AARON", "Kimberlei", 1000);
-    add("ADEBAYO", "Benjamin", 1000);
-    add("ADEBAYO", "Benjamin", 1000);
-   //printf("%s\n", table.vecteur[index].content[0].name);
-   //printf("%s\n", table.vecteur[index].content[1].name);
-  load(3);
-  /*
-  for (int i =0; i<100;i++)
+  //  add("AARON", "Kimberlei", 1000);
+// add("ADEBAYO", "Benjamin", 1000);
+// add("ADEBAYO", "Benjamin", 1000);
+  // printf("%s\n", table.vecteur[index].content[0].name);
+  //printf("%s\n", table.vecteur[index].content[1].name);
+ load(3);
+  
+  for (int i =0; i< table.size;i++)
   {
       if (table.vecteur[i].content != NULL)
       {
-          printf("%d : %s \n", i, table.vecteur[index].content[0].name);
+          if (table.vecteur[i].content[0].name == NULL)
+          {
+              
+          }
+          else
+          {
+            
+          printf("\n %d : %s \n", i, table.vecteur[i].content[0].name);
+      }
       }
   }
-  */
+  
    //printf("%s\n", table.vecteur[index].content[table.vecteur[index].logic_size].surname);
    //printf("%d\n", table.vecteur[index].content[table.vecteur[index].logic_size].salary);
 }
--- Projet_Langage/table.c
+++ Projet_Langage/table.c
@@ -66,9 +66,9 @@ void add(char* name, char* surname, unsigned int salary)
     if (table.vecteur[index].content == NULL)
     {
         table.vecteur[index].content= malloc(sizeof(civil_servant));
-        table.vecteur[index].content[table.vecteur[index].logic_size].name = name;
-        table.vecteur[index].content[table.vecteur[index].logic_size].surname = surname;
-        table.vecteur[index].content[table.vecteur[index].logic_size].salary = salary;
+        table.vecteur[index].content[0].name = name;
+        table.vecteur[index].content[0].surname = surname;
+        table.vecteur[index].content[0].salary = salary;
         table.vecteur[index].physical_size++;
         table.vecteur[index].logic_size++;
     }
@@ -92,19 +92,12 @@ void add(char* name, char* surname, unsigned int salary)
         }
         if (table.vecteur[index].physical_size > table.vecteur[index].logic_size)        
         {
-
         table.vecteur[index].content[table.vecteur[index].logic_size].name = name;
         table.vecteur[index].content[table.vecteur[index].logic_size].surname = surname;
         table.vecteur[index].content[table.vecteur[index].logic_size].salary = salary;
         table.vecteur[index].logic_size++;
-            
-            
-            
         }
-        
     }
-       
-    
 }
 int show_salary(char* name, char* surname)
 {
@@ -120,13 +113,16 @@ int show_salary(char* name, char* surname)
     return salary;
 }
 
+
+
 void load(int number_of_servant)
 {
     int TAILLE_MAX = 1000;
     FILE* fichier = NULL;
     char chaine[TAILLE_MAX];
-    char* name;// = ( char *) malloc(20* sizeof(char));
-    char * surname;// = ( char *)malloc(20*sizeof(char));
+    char* alloc_name;// = (char*) malloc(20);
+    char* alloc_surname;// = (char*) malloc(20);
+ //   char * alloc_surname = malloc(20*sizeof(char));
    // int salary;
 
     fichier = fopen("chicago.txt", "r");
@@ -135,23 +131,136 @@ int tour = 0;
  
     if (fichier != NULL)
     {
-        while (fgets(chaine, TAILLE_MAX, fichier) != NULL && tour <1)  // On lit le fichier tant qu'on ne reçoit pas d'erreur (NULL)
+        while (fgets(chaine, TAILLE_MAX, fichier) != NULL)  // On lit le fichier tant qu'on ne reçoit pas d'erreur (NULL)
         {
-                      //  name = ( char *) malloc(20* sizeof(char));
-                    //   surname = ( char *) malloc(20* sizeof(char));
+if (tour <10)
+{
+    
+            alloc_name = (char*) malloc(20);
+            alloc_surname = (char*) malloc(20);
             civil_servant * new_civil = malloc(sizeof(civil_servant));
             //fscanf(fichier, "%s %s %d", name, surname, &new_civil->salary);
-            fscanf(fichier, "%s %s %d", name, surname, &new_civil->salary);
+            fscanf(fichier, "%s %s %d", alloc_name, alloc_surname, &new_civil->salary);
        //     fscanf(fichier, "%s %s %u", name, surname, &new_civil->salary);
-            //printf("%s",name);
-            add(name, surname,new_civil->salary);
+            printf("\ntour n°%d : %s",tour+1, alloc_name);
+           // printf("\ntour n°%d : %s",tour+1, alloc_surname);
+            //printf("\ntour n°%d : %d",tour+1, new_civil->salary);
+            printf("\ntour n°%d - index  %d",tour+1, index_calculator(alloc_name, alloc_surname));
+            new_civil->name = alloc_name;
+            new_civil->surname = alloc_surname;
+            add(new_civil->name, new_civil->surname, new_civil->salary);
+            
+            //alloc_surname =  malloc(20*sizeof(char));
+          //  add(name, surname,new_civil->salary);
             tour++;
             //free(name); free(surname);
-            name = ( char *) malloc(20* sizeof(char));
            // name = ( char *) malloc(20* sizeof(char));
+        //   name = malloc(20*sizeof(char));
+         //   surname = malloc(20*sizeof(char));
         }
+        else
+        {
+            break;
+    }
+    }
     }  
         fclose(fichier);
-
 }
 
+/*
+void load (int number_of_servant)
+{
+    char name[20]; // = malloc(20*sizeof(char));
+        char* test = "carol"; 
+        sprintf(name, "%s", test);
+        printf("name test : %s", name);
+        test = "v2";
+             sprintf(name, "%s", test);
+       printf("name test : %s", name);
+}
+*/
+/*  
+void load(int number_of_servant)
+{
+    int TAILLE_MAX = 30;
+    FILE* fichier = NULL;
+    char chaine[TAILLE_MAX];
+    char *name = malloc(20* sizeof(char));
+    char *surname = malloc(20*sizeof(char));
+   // char surname[20]; //= malloc(sizeof(char));   
+    fichier = fopen("chicago.txt", "r");
+  //for (int i = 0; )
+int tour = 0;
+ 
+    if (fichier != NULL)
+    {
+        while (fgets(chaine, TAILLE_MAX, fichier) != NULL)// On lit le fichier tant qu'on ne reçoit pas d'erreur (NULL)
+    {
+        for (int i =0; i<1; i++)
+        {
+            
+                civil_servant * new_civil = malloc(sizeof(civil_servant));
+                fscanf(fichier, "%s %s %u", name, surname, &new_civil->salary);
+                //new_civil->name = name;
+                //new_civil->surname = surname;
+                tour++;
+                printf("%d : %s", i, name );
+               // printf("%s ", new_civil->name);
+               // printf("%s \n", new_civil->surname);
+                //int i = index_calculator(name,surname);
+                //printf("%d", i);
+                
+            }
+        }
+            
+}    
+
+  
+        fclose(fichier);
+    }
+    */
+/*
+void load(int number_of_servant)
+{
+    char nom_fichier[] = "Chicago.txt";
+	FILE* fichier = fopen(nom_fichier,"r"); // ouverture en lecture (read)
+   char name[20]; //= malloc(sizeof(char));
+    //char surname[20]; //= malloc(sizeof(char));
+    //char* read_name;
+  //  char* read_surname;
+  char test[1];
+    int tour =0;
+	if( fichier != NULL )
+	{
+		char c = fgetc(fichier); // lecture du 1er caractère dans fichier, 
+        printf(" le char est : %c", c);
+             // puis fgetc se positionne sur le caractère suivant (pour la proichaine lecture)
+		
+		while( c != EOF && tour <1)   // tant que le caractère lu n'est pas la fin de fichier (End Of File)
+		{
+            int i =0;
+            test[0] =c;
+            while( test[0] =! ' ' )
+            {
+                
+                name[i] = c;
+                i++;
+                c = fgetc(fichier);
+                test[0] =c;
+            }
+            name[i+1] = '\0';
+            tour++;
+            printf("\nle nom est : %s",name);
+            
+			//printf("%c", c);
+			c = fgetc(fichier); // lecture du caractère suivant
+		}
+		
+		fclose(fichier); // fermeture du fichier
+	}
+	else
+	{
+		printf("Problème lors de l'ouverture du fichier %s\n", nom_fichier);
+	}
+}
+*/
